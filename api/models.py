be0e6a896367d911588a59ae51606f6d49ed2f39
SamanNaruee <narueesaman@gmail.com>
2025-03-17 06:17:43 +0330

    add product and Order models.

--- api/models.py
+++ api/models.py
@@ -5,3 +5,14 @@ class Farmer(models.Model):
     farmer_key = models.UUIDField(primary_key=True, editable=False, unique=True)
     token = models.UUIDField(max_length=1024)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+
+class Order(models.Model):
+    order_id = models.UUIDField(primary_key=True)
+    status = models.CharField(max_length=50)
+    redirect_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
